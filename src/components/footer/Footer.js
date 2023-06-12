8874377d6b43ed098de6ffd465e5953bd9933ca3
niNovikov <gambitrus38@yandex.ru>
2023-06-12 22:46:29 +0300

    fix footer links

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,16 +1,17 @@
 import React from "react";
 import './Footer.css';
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="copyright">
-        <p>© 2020 - 2023 <a href="/" target="_blank">enot.dev</a></p>
+        <p>© 2020 - 2023 <a href="https://github.com/Space8rain" target="_blank">SpaceBrain</a></p>
         <p>Режимы работы склонов и цены на подъемники не являются публичной офертой.</p>
       </div>
       <ul className="footer_link">
-        <li className="footer_icon tg"></li>
-        <li className="footer_icon vk"></li>
+          <a href="https://t.me/cwraccoon" target="blank" className="footer_icon tg"></a>
+        {/* <li className="footer_icon vk"></li> */}
       </ul>
     </footer>
   )
