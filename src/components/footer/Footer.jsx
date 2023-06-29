99b24b36937a9d48a53db7c57d9de77531217b97
niNovikov <gambitrus38@yandex.ru>
2023-06-30 00:03:51 +0300

    ref footer & scroll button

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,11 +5,18 @@ function Footer() {
   return (
     <footer className="footer">
       <div className="copyright">
-        <p>© 2023 <a href="https://github.com/Space8rain" target="_blank">SpaceBrain</a></p>
+        <p>© 2023</p>
         <p>Режимы работы склонов и цены на подъемники не являются публичной офертой.</p>
       </div>
       <ul className="footer_links">
-          <a href="https://t.me/cwraccoon" target="blank" className="footer_icon tg"></a>
+        <li className="footer_link">
+          <a href="https://t.me/cwraccoon" rel="noreferrer" target="_blank" className="footer_icon tg"> </a>
+          <p>back</p>
+        </li>
+        <li className="footer_link">
+          <a href="https://github.com/Space8rain" rel="noreferrer" target="_blank" className="footer_icon gh"> </a>
+          <p>front</p>
+        </li>
         {/* <li className="footer_icon vk"></li> */}
       </ul>
     </footer>
